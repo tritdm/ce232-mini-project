e12b466f377e060b9d2f621be8a0b194e4959aff
tritdm <20520326@gm.uit.edu.vn>
2023-06-12 20:46:21 +0700

    update ESP32 source

--- esp32_source/main/src/main.c
+++ esp32_source/main/src/main.c
@@ -1,12 +1,3 @@
-/* MQTT (over TCP) Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
 #include <stdio.h>
 #include <stdint.h>
 #include <stddef.h>
@@ -35,8 +26,6 @@
 #include "hdc1080.h"
 #include "i2c_init.h"
 #include "oled_string_process.h"
-//#include "ssd1306.h"
-//#include "font8x8_basic.h"
 #include "oled_task.h"
 
 #define MQTT_BROKER_URI             "mqtt://mqtt.flespi.io"
@@ -51,10 +40,6 @@ static esp_mqtt_client_handle_t client;
 hdc1080_registers_t *sensor;
 
 const char *TAG = "MINI_PROJECT";
-
-/*void ssd1306_init(void);
-void task_ssd1306_display_text(const char *text);
-void task_ssd1306_display_clear(void *ignore);*/
 
 static void log_error_if_nonzero(const char *message, int error_code)
 {
@@ -192,110 +177,3 @@ void app_main(void)
 		free(oled_print);
 	}
 }
-/*
-void ssd1306_init(void)
-{
-	i2c_cmd_handle_t cmd;
-	cmd = i2c_cmd_link_create();
-
-	i2c_master_start(cmd);
-	i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
-	i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
-
-	i2c_master_write_byte(cmd, OLED_CMD_SET_CHARGE_PUMP, true);
-	i2c_master_write_byte(cmd, 0x14, true);
-
-	i2c_master_write_byte(cmd, OLED_CMD_SET_SEGMENT_REMAP, true); // reverse left-right mapping
-	i2c_master_write_byte(cmd, OLED_CMD_SET_COM_SCAN_MODE, true); // reverse up-bottom mapping
-
-	i2c_master_write_byte(cmd, OLED_CMD_DISPLAY_NORMAL, true);
-    i2c_master_write_byte(cmd, OLED_CMD_DISPLAY_OFF, true);
-	i2c_master_write_byte(cmd, OLED_CMD_DISPLAY_ON, true);
-	i2c_master_stop(cmd);
-
-	i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 10 / portTICK_PERIOD_MS);
-	i2c_cmd_link_delete(cmd);
-}
-
-void task_ssd1306_display_text(const char *text) 
-{
-	//char *text = (char*)arg_text;
-	uint8_t text_len = strlen(text);
-
-	i2c_cmd_handle_t cmd;
-
-	uint8_t cur_page = 0;
-
-	cmd = i2c_cmd_link_create();
-	i2c_master_start(cmd);
-	i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
-
-	i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
-	i2c_master_write_byte(cmd, 0x00, true); 
-	i2c_master_write_byte(cmd, 0x10, true);
-	i2c_master_write_byte(cmd, 0xB0 | cur_page, true);
-
-	i2c_master_stop(cmd);
-	i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 10 / portTICK_PERIOD_MS);
-	i2c_cmd_link_delete(cmd);
-
-	for (uint8_t i = 0; i < text_len; i++) {
-		if (text[i] == '\n') 
-		{
-			cmd = i2c_cmd_link_create();
-			i2c_master_start(cmd);
-			i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
-
-			i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
-			i2c_master_write_byte(cmd, 0x00, true); 
-			i2c_master_write_byte(cmd, 0x10, true);
-			i2c_master_write_byte(cmd, 0xB0 | ++cur_page, true); 
-
-			i2c_master_stop(cmd);
-			i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 10 / portTICK_PERIOD_MS);
-			i2c_cmd_link_delete(cmd);
-		} 
-		else 
-		{
-			cmd = i2c_cmd_link_create();
-			i2c_master_start(cmd);
-			i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
-
-			i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_DATA_STREAM, true);
-			i2c_master_write(cmd, font8x8_basic_tr[(uint8_t)text[i]], 8, true);
-
-			i2c_master_stop(cmd);
-			i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 10 / portTICK_PERIOD_MS);
-			i2c_cmd_link_delete(cmd);
-		}
-	}
-	vTaskDelete(NULL);
-}
-
-void task_ssd1306_display_clear(void *ignore) 
-{
-	i2c_cmd_handle_t cmd;
-
-	uint8_t clear[128];
-	for (uint8_t i = 0; i < 128; i++) 
-	{
-		clear[i] = 0;
-	}
-	for (uint8_t i = 0; i < 8; i++) 
-	{
-		cmd = i2c_cmd_link_create();
-		i2c_master_start(cmd);
-		i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
-		i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_SINGLE, true);
-		i2c_master_write_byte(cmd, 0xB0 | i, true);
-
-		i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_DATA_STREAM, true);
-		i2c_master_write(cmd, clear, 128, true);
-		i2c_master_stop(cmd);
-		i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 10 / portTICK_PERIOD_MS);
-		i2c_cmd_link_delete(cmd);
-	}
-
-	vTaskDelete(NULL);
-}
-*/
